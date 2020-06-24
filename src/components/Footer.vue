d0088d2504ad828460e417ebcb3400e7f6e40fd6
Achilles45 <achillesusuoyibo7@gmail.com>
2020-06-25 00:34:06 +0100

    Finalized the project

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -29,7 +29,7 @@
           <div class="container">
               <div class="row">
                   <div class="col-12">
-                      <p>Pectroleum Investment Company &copy; All Right Reserved, 2020</p>
+                      <p>Courage Earners Company &copy; All Right Reserved, 2020</p>
                   </div>
               </div>
           </div>
